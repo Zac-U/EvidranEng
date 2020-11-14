f29392044396ca4b5df6209ab47a86ee50caa625
Peter Marsh <breadloaf64@gmail.com>
2020-11-14 23:22:35 +0000

    Done some very vague stuff for buttons

--- src/game/Menu/Button.java
+++ src/game/Menu/Button.java
@@ -1,7 +1,26 @@
 package game.Menu;
 
 import engine.entity.entity;
+import engine.event.windowEvent;
 
 public class Button extends entity {
+    String text;
 
+    Button(int x, int y, int volx, int voly) {
+        super(x, y, volx, voly);
+    }
+
+    public void setText(String newText) {
+        text = newText;
+    }
+
+    @Override
+    public void tick(game g) {
+
+    }
+
+    @Override
+    public void mouseClick(windowEvent E) {
+        //change scene somehow
+    }
 }
