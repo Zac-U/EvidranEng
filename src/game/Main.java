2c881103ef61ff7f953da6e23d6cc2b36382a488
Zac <rs19764@bristol.ac.uk>
2020-11-14 23:45:08 +0000

    moving wall

--- src/game/Main.java
+++ src/game/Main.java
@@ -9,11 +9,13 @@ public class Main {
     public static tick.game main() {
         GameFactory maker = new GameFactory();
         player p = new player(500, 500,50,50);
+        wall w = new wall(1000,10);
         Level l = new Level();
         l.addEntity(p);
         l.setLastDrawn(p);
         l.addKeyPressed(p);
         l.addKeyReleased(p);
+        l.addEntity(w);
         Stage st = new Stage("Stage_One");
 
         st.setLevel(l);
